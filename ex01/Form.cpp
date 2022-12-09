fd67263873afe1478eefa8e69e791c57d21469f0
Nuno Louro <nlouro@student.42heilbronn.de>
2022-12-09 19:39:15 +0100

    Add forgotten Form attribute _grade_4exec

--- ex01/Form.cpp
+++ ex01/Form.cpp
@@ -6,15 +6,15 @@
 /*   By: nlouro <nlouro@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 09:58:44 by nlouro            #+#    #+#             */
-/*   Updated: 2022/12/09 19:17:43 by nlouro           ###   ########.fr       */
+/*   Updated: 2022/12/09 19:31:11 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Form.hpp"
 
-Form::Form( std::string name, size_t grade_4sign ) : _name(name), _grade_4sign(grade_4sign), _signed(false)
+Form::Form( std::string name, size_t grade_4sign, size_t grade_4exec ) : _name(name), _grade_4sign(grade_4sign), _grade_4exec(grade_4exec), _signed(false)
 {
-	std::cout << this->_name << " (Form) created with grade " << this->_grade_4sign << std::endl;
+	std::cout << this->_name << " (Form) created with grade for sign " << this->_grade_4sign << " and grade for exec " << this->_grade_4exec << std::endl;
 }
 
 Form::~Form( void )
@@ -22,7 +22,7 @@ Form::~Form( void )
 	std::cout << this->_name << " (Form) destructor called." << std::endl;
 }
 
-Form::Form( const Form &src ) : _name(src._name), _grade_4sign(src._grade_4sign)
+Form::Form( const Form &src ) : _name(src._name), _grade_4sign(src._grade_4sign), _grade_4exec(src._grade_4exec)
 {
 	std::cout << this->_name << " (Form) copied." << std::endl;
 }
@@ -32,7 +32,7 @@ Form & Form::operator=( const Form &src )
 	//this->_name = src.getName();
 	//this->_grade_4sign = src._grade_4sign;
 	this->_signed = src._signed;
-	std::cout << "Form class partially assigned. const name and grade can't be re-assigned\n";
+	std::cout << "Form class partially assigned. const name and grades can't be re-assigned\n";
 	return *this;
 }
 
@@ -60,10 +60,19 @@ bool	Form::beSigned( Bureaucrat &b )
 		throw Form::GradeTooHighException();
 }
 
+size_t	Form::getGrade4Exec( void ) const
+{
+	return this->_grade_4exec;
+}
+
+std::string	Form::getGrade4ExecAsString( void ) const
+{
+	return std::to_string( this->getGrade4Exec() );
+}
 
 std::ostream & operator<<( std::ostream &ostream, Form const &f)
 {
-	return ostream << f.getName() << " requires grade >" << f.getGrade4SignAsString() << " for signature";
+	return ostream << f.getName() << " requires grade for sign >" << f.getGrade4SignAsString() << " for signature";
 }
 
 const char* Form::GradeTooHighException::what( void ) const throw()
--- ex01/Form.hpp
+++ ex01/Form.hpp
@@ -6,7 +6,7 @@
 /*   By: nlouro <nlouro@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 17:10:29 by nlouro            #+#    #+#             */
-/*   Updated: 2022/12/09 19:16:49 by nlouro           ###   ########.fr       */
+/*   Updated: 2022/12/09 19:22:41 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,18 @@ class	Form
 	private:
 		std::string	const	_name;
 		size_t	const 		_grade_4sign;
+		size_t	const 		_grade_4exec;
 		bool				_signed;
 	public:
-		Form( std::string name, size_t grade );
+		Form( std::string name, size_t grade_4sign, size_t grade_4exec);
 		Form( const Form &src );
 		Form &operator=( const Form &src );
 		~Form();
 		std::string	getName( void ) const;
 		size_t		getGrade4Sign( void ) const;
+		size_t		getGrade4Exec( void ) const;
 		std::string	getGrade4SignAsString( void ) const;
-		size_t		incrementGrade ( void );
-		size_t		decrementGrade ( void );
+		std::string	getGrade4ExecAsString( void ) const;
 		bool		beSigned( Bureaucrat &b );
 		// Class Exceptions
 		class   GradeTooHighException : public std::exception
